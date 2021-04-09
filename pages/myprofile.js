20b43216c117c61d8a1a9b6e643514c4a78b1ba3
Robert Mariuson <gigglefeet@gmail.com>
2021-04-09 21:16:15 +0200

    layout fix

--- pages/myprofile.js
+++ pages/myprofile.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 export default function home() {
   return (
-    <>
-      <div>
-        <h1>My Profile</h1>
-        <Image
-          src="/training.png"
-          alt="picture of a deadlifter getting ready to lift"
-          width={650}
-          height={500}
-        />
-        <p>Welcome to your profile!</p>
-      </div>
-    </>
+    <div>
+      <h1>My Profile</h1>
+      <Image
+        src="/training.png"
+        alt="picture of a deadlifter getting ready to lift"
+        width={650}
+        height={500}
+      />
+      <p>Welcome to your profile!</p>
+    </div>
   );
 }
