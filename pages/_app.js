dd88b3f76eb8b6ea81fe4610f90a7bce0e1920b4
Robert Mariuson <gigglefeet@gmail.com>
2021-04-09 21:38:43 +0200

    fixed globalstyles version 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
-import { globalStyles } from '../shared/globalStyles';
+import { globalStyles } from '../shared/globalstyles';
 export default function App({ Component, pageProps }) {
   const [isSessionStateStale, setIsSessionStateStale] = useState(true);
 
